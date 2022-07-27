54e5c462586a3aa28238c938b0224c06e062f663
JoelTejerina <84384954+JoelTejerina@users.noreply.github.com>
2022-07-26 22:59:39 -0300

    Delete java/edu/curso/java/ejemplolog4j directory

--- java/edu/curso/java/ejemplolog4j/GestorDeArchivosException.java
+++
@@ -1,9 +0,0 @@
-package edu.curso.java.ejemplolog4j;
-
-public class GestorDeArchivosException extends Exception {
-
-	public GestorDeArchivosException(String msg, Throwable errorOriginal) {
-		super(msg, errorOriginal);
-	}
-
-}
